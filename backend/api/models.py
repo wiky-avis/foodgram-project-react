15e8eb5d96dd6f02ed4c4f7dbef13a3433e58fe9
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-09-18 15:37:46 +0300

    рыба модели Тег

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    title = models.CharField('Название', max_length=70)
+    hexcolor = models.CharField('Цветовой HEX-код', max_length=7, default="#49B64E")
+    slug = models.SlugField('Slug')
+
+
 class Recipe(models.Model):
     author = models.CharField('Автор публикации (пользователь)', max_length=70)
     title = models.CharField('Название', max_length=70)
     image = models.ImageField('Картинка')
     description = models.TextField('Текстовое описание')
     ingredients = models.CharField('Ингредиенты', max_length=70)
-    tag = models.CharField('Тег', max_length=70)
+    tag = models.ManyToManyField(Tag, verbose_name="Тег")
     cooking_time_min = models.PositiveIntegerField(
         'Время приготовления в минутах'
     )
