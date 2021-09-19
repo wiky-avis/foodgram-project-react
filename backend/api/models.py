97e97ea28b59b31a4653106d7b1d95404238c75a
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-09-19 18:44:51 +0300

    модель рецепта

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,26 +1,4 @@
 from django.db import models
 
 
-class Tag(models.Model):
-    title = models.CharField('Название', max_length=70)
-    hexcolor = models.CharField('Цветовой HEX-код', max_length=7, default="#49B64E")
-    slug = models.SlugField('Slug')
-
-
-class Ingredient(models.Model):
-    title = models.CharField('Название', max_length=70)
-    quantity = models.PositiveIntegerField('Количество')
-    units_measurement = models.CharField('Единицы измерения', max_length=10)
-
-
-class Recipe(models.Model):
-    author = models.CharField('Автор публикации (пользователь)', max_length=70)
-    title = models.CharField('Название', max_length=70)
-    image = models.ImageField('Картинка')
-    description = models.TextField('Текстовое описание')
-    ingredients = models.ManyToManyFiel(Ingredient, verbose_name='Ингредиенты')
-    tag = models.ManyToManyField(Tag, verbose_name="Тег")
-    cooking_time_min = models.PositiveIntegerField(
-        'Время приготовления в минутах'
-    )
 
---
+++ backend/recipes/models.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+from django.db import models
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    title = models.CharField('Название', max_length=50, db_index=True)
+    hexcolor = models.CharField(
+        'Цветовой HEX-код', max_length=7, default="#49B64E"
+    )
+    slug = models.SlugField('Slug', max_length=50, unique=True)
+
+    def __str__(self):
+        return f'Тег - {self.title}'
+
+
+class Ingredient(models.Model):
+    title = models.CharField('Название', max_length=70)
+    quantity = models.PositiveIntegerField('Количество')
+    units_measurement = models.CharField('Единицы измерения', max_length=10)
+
+    def __str__(self):
+        return f'{self.title} - {self.quantity} - {self.units_measurement}'
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации (пользователь)'
+    )
+    title = models.CharField('Название', max_length=255)
+    image = models.ImageField('Картинка', upload_to='recipe/')
+    description = models.TextField('Описание')
+    ingredients = models.ManyToManyField(
+        Ingredient, verbose_name='Ингредиенты', blank=True
+    )
+    tag = models.ManyToManyField(
+        Tag, related_name='recipe_tag', blank=True, verbose_name='Тег'
+    )
+    cooking_time_min = models.PositiveIntegerField(
+        'Время приготовления в минутах', validators=[MinValueValidator(1)]
+    )
+
+    def __str__(self):
+        return f'Рецепт - {self.title}'
