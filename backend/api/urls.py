2672b30986f45ee40a27ffb2639ca88d7e1e7afa
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-09-19 20:22:24 +0300

    добавлены модели Избранные рецепты, Подписки, Список покупок

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,23 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 v1_router = DefaultRouter()
+# v1_router.register('users', UserViewSet)
+# v1_router.register(
+#     r'users/(?P<title_id>\d+)', UserProfileViewSet,)
+# v1_router.register(
+#     r'users/(?P<title_id>\d+)/subscribe', SubscribeViewSet)
+# v1_router.register('tags', TagViewSet)
+# v1_router.register('tags/(?P<title_id>\d+)', TagDetailViewSet)
+# v1_router.register('recipes', RecipeViewSet)
+# v1_router.register('recipes/(?P<title_id>\d+)', ResipeDetailViewSet)
+# v1_router.register('recipes/(?P<title_id>\d+)/shopping_cart', ShoppingCartViewSet)
+# v1_router.register('recipes/(?P<title_id>\d+)/favorite', FavoriteViewSet)
+# v1_router.register('ingredients', UserViewSet)
+# v1_router.register(
+#     r'ingredients/(?P<title_id>\d+)', UserProfileViewSet)
 
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
+    path('', include(v1_router.urls)),
+    # path('auth/token/login', Joser.as_view(), name='get_token'),
 ]
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -6,23 +6,23 @@ User = get_user_model()
 
 
 class Tag(models.Model):
-    title = models.CharField('Название', max_length=50)
-    hexcolor = models.CharField(
+    name = models.CharField('Название', max_length=50, db_index=True)
+    color = models.CharField(
         'Цветовой HEX-код', max_length=7, default="#49B64E"
     )
     slug = models.SlugField('Slug', max_length=50, unique=True)
 
     def __str__(self):
-        return f'Тег - {self.title}'
+        return f'Тег - {self.name}'
 
 
 class Ingredient(models.Model):
-    title = models.CharField('Название', max_length=70)
+    name = models.CharField('Название', max_length=70, db_index=True)
     quantity = models.PositiveIntegerField('Количество')
-    units_measurement = models.CharField('Единицы измерения', max_length=10)
+    measurement_unit = models.CharField('Единицы измерения', max_length=10)
 
     def __str__(self):
-        return f'{self.title} - {self.quantity} - {self.units_measurement}'
+        return f'{self.name} - {self.quantity} - {self.measurement_unit}'
 
 
 class Recipe(models.Model):
@@ -31,18 +31,45 @@ class Recipe(models.Model):
         on_delete=models.CASCADE,
         verbose_name='Автор публикации (пользователь)'
     )
-    title = models.CharField('Название', max_length=255)
+    name = models.CharField('Название', max_length=255)
     image = models.ImageField('Картинка', upload_to='recipe/')
-    description = models.TextField('Описание')
+    text = models.TextField('Описание')
     ingredients = models.ManyToManyField(
-        Ingredient, verbose_name='Ингредиенты'
+        Ingredient,
+        verbose_name='Ингредиенты',
+        related_name='recipe_ingredients'
     )
-    tag = models.ManyToManyField(
-        Tag, related_name='recipe_tag', blank=True, verbose_name='Тег'
+    tags = models.ManyToManyField(
+        Tag, related_name='recipe_tags', blank=True, verbose_name='Теги'
     )
-    cooking_time_min = models.PositiveIntegerField(
+    cooking_time = models.PositiveIntegerField(
         'Время приготовления в минутах', validators=[MinValueValidator(1)]
     )
 
     def __str__(self):
-        return f'Рецепт - {self.title}'
+        return f'Рецепт - {self.name}'
+
+
+class FavoriteRecipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='users_favorite')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='favorite_recipes')
+
+
+class Subscribe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='follower')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='following')
+
+    def __str__(self):
+        return f'{self.pk} - {self.user} - {self.author}'
+
+
+class ShoppingList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, )
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, )
+
+    def __str__(self):
+        return f'{self.pk} - {self.user} - {self.recipe}'
