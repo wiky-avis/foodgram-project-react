e1fcb2b059940e23263ed0b09c44696b7992d7e6
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-10-02 18:41:19 +0300

    workflow

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -70,13 +70,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ['DB_ENGINE'],
