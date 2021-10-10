bcab14ef436c4279a787d82c2d0726bf93ce7031
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-10-10 19:17:06 +0300

    test api users 3

--- backend/tests/test_api_users.py
+++ backend/tests/test_api_users.py
@@ -7,9 +7,30 @@ class TestApiUser:
 
     @pytest.mark.django_db(transaction=True)
     @pytest.mark.parametrize(
+        'email, username, first_name, last_name, password, status_code', [
+            (None, None, None, None, None, HTTPStatus.BAD_REQUEST),
+            ('test1@test.ru', None, None, None, 'user_pass', HTTPStatus.BAD_REQUEST),
+            ('test2@test.ru', 'user_test', 'Anna', None, 'user_pass', HTTPStatus.BAD_REQUEST),
+            ('test3@test.ru', 'user_test2', 'Vasya', 'Pupkin', 'user_pass', HTTPStatus.CREATED)
+        ])
+    def test_registration_user(
+            self, api_client, email, username, first_name, last_name, password, status_code
+    ):
+        data = {
+            'email': email,
+            'username': username,
+            'first_name': first_name,
+            'last_name': last_name,
+            'password': password
+        }
+        response = api_client.post('/api/users/', data=data, format='json')
+        assert response.status_code == status_code
+
+    @pytest.mark.django_db(transaction=True)
+    @pytest.mark.parametrize(
         "data, http_status", [
             ({'email': 'test_user@foodgram.fake', 'password': '1234567'}, HTTPStatus.CREATED),
-            ({'email': '', 'password': ''}, HTTPStatus.UNAUTHORIZED)
+            ({'email': None, 'password': None}, HTTPStatus.UNAUTHORIZED)
         ]
     )
     def test_users_get_token(self, user_client, data, http_status):
@@ -25,24 +46,3 @@ class TestApiUser:
         }
         response = admin_client.post('/api/auth/token/login/', data=data)
         assert response.status_code == HTTPStatus.CREATED
-
-    @pytest.mark.django_db(transaction=True)
-    @pytest.mark.parametrize(
-        'email, username, first_name, last_name, password, status_code', [
-            (None, None, None, None, None, HTTPStatus.BAD_REQUEST),
-            ('test1@test.ru', None, None, None, 'user_pass', HTTPStatus.BAD_REQUEST),
-            ('test2@test.ru', 'user_test', 'Anna', None, 'user_pass', HTTPStatus.BAD_REQUEST),
-            ('test3@test.ru', 'user_test2', 'Vasya', 'Pupkin', 'user_pass', HTTPStatus.CREATED)
-            ])
-    def test_registration_user_and_get_token(
-            self, api_client, email, username, first_name, last_name, password, status_code
-    ):
-        data = {
-            'email': email,
-            'username': username,
-            'first_name': first_name,
-            'last_name': last_name,
-            'password': password
-        }
-        response = api_client.post('/api/users/', data=data, format='json')
-        assert response.status_code == status_code
