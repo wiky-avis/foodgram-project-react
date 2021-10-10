4dd0afff3c4df6d4944313b3b6b1aa27c4ac9f2d
wiky-avis <peta-holding@yandex.ru>
2021-10-10 13:18:35 +0300

    pytest

--- backend/tests/test_api_tags.py
+++ backend/tests/test_api_tags.py
@@ -9,3 +9,22 @@ class TestApiTags:
     def test_list_tags(self, client):
         response = client.get('/api/tags/')
         assert response.status_code == 200
+
+        data = response.json()
+
+        data_2 = [
+            {
+                "id": 1,
+                "name": "Борщ",
+                "color": "#ffffff",
+                "slug": "borsh"
+                },
+            {
+                "id": 2,
+                "name": "Суп",
+                "color": "#ff00ff",
+                "slug": "sup"
+                }
+            ]
+
+        assert data == data_2
